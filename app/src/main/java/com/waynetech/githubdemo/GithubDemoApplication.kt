5b8fa00c4d56c0be1faea01234dfa3bb74d9b450
Harsh Sharma <harsh.sharma@honestbee.com>
2019-05-21 01:23:11 +0530

    Add layout for activity and item

---
+++ app/src/main/java/com/waynetech/githubdemo/GithubDemoApplication.kt
@@ -0,0 +1,20 @@
+package com.waynetech.githubdemo
+
+import android.app.Application
+import com.waynetech.githubdemo.di.AppModule
+import com.waynetech.githubdemo.di.DaggerAppComponent
+import com.waynetech.githubdemo.di.NetworkModule
+
+class GithubDemoApplication : Application() {
+
+    private val appComponent = DaggerAppComponent
+        .builder()
+        .appModule(AppModule(this))
+        .networkModule(NetworkModule())
+        .build()
+
+    override fun onCreate() {
+        super.onCreate();
+        appComponent.inject(this)
+    }
+}
